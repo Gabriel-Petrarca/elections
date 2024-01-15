6fdb4dc8b6fc84a332943ccdda8c8223a82f01fa
spangenberg-a <aspanmav@gmail.com>
2024-01-15 16:16:11 -0500

    president page commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import President from './pages/President';
 import Home from './pages/home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/president" element={<President/>} />
       </Routes>
       </Router>
     </div>
--- Frontend/src/pages/President.js
+++ Frontend/src/pages/President.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/votescreen.css';
 
 function President() {
+  const Candidate1 = "Candidate 1";
+  const Candidate2 = "Candidate 2";
+  const Candidate3 = "Candidate 3";
+  const Candidate4 = "Candidate 4";
+
   return (
-    <div>
-      
+    <div className="President">
+      <div className="headerContainer">
+        <h1>Vote for President</h1>
+        <div className="candidates">
+          <button>{Candidate1}</button>
+          <button>{Candidate2}</button>
+          <button>{Candidate3}</button>
+          <button>{Candidate4}</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default President
+export default President;
