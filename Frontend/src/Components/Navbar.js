aaae3bf5640edbedc251bd425aa4561af5150174
spangenberg-a <aspanmav@gmail.com>
2024-01-19 13:11:31 -0500

    updated log out front end

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -1,17 +1,46 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import SAClogo from '../assets (images)/SAClogowhite.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../Styles/Navbar.css'
+
+
+/* creates all of the links at the top of the screen in the red box*/
+/* creates the drop down menu icon that appears when the screen is a smaller size*/
+/* includes the log out button that redirects users back to the login page */
 
 function Navbar() {
 
     const [openLinks, setOpenLinks] = useState(false)
+    const navigate = useNavigate();
 
     const toggleNavbar = () =>{
         setOpenLinks(!openLinks)
     };
 
+    function logout(){
+      navigate('/login');
+    }
+
+    /* handles the resize of the window when it goes back and forth between small and large screen */
+    useEffect(() => {
+      const handleResize = () => {
+        const screenWidth = window.innerWidth;
+        if (screenWidth > 600) {
+          setOpenLinks(false);
+        }
+      };
+      
+      window.addEventListener('resize', handleResize);
+      
+      handleResize();
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+    /* creates the links and the logo that appear at the top of the different size screens */
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
@@ -25,6 +54,7 @@ function Navbar() {
             <Link to="/marketing"> Marketing </Link>
             <Link to="/finance"> Finance </Link>
             <Link to="/i&b"> I&B </Link>
+            
         </div>
       </div>
       <div className="rightSide">
@@ -37,9 +67,10 @@ function Navbar() {
         <Link to="/finance"> Finance </Link>
         <Link to="/i&b"> I&B </Link>
         <button onClick={toggleNavbar}>
-        <ReorderIcon />
+          <ReorderIcon />
         </button>
-        </div>
+      </div>
+      <button onClick={logout}>Log Out</button>
     </div>
   )
 }
